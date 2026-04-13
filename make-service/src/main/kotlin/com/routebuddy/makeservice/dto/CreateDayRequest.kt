f9baef54c479530953c7c42332fffd1fa8c362e4
jshustrik <j.shustrik@gmail.com>
2026-04-13 22:57:47 +0300

    add some dto, add make service for days, routes and etc.

--- make-service/src/main/kotlin/com/routebuddy/makeservice/dto/CreateDayRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.routebuddy.makeservice.dto
-
-data class CreateDayRequest(
-    val dayNumber: Int,
-    val title: String,
-    val description: String,
-    val cost: Double,
-    val points: List<CreatePointRequest> = emptyList()
-)
